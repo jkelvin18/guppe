72cd92ade465a62286c32bdaf0037f369bff24bb
jkelvin18 <jkelvin18@hotmail.com>
2020-10-19 23:46:00 -0300

    Incluido documentacao de listas

--- listas.py
+++ listas.py
@@ -1,5 +1,16 @@
 """
 Listas
 
-Listas em python
+Listas em Python funcional com vetores/matrizes (arrays) em outras linguagens, com a diferenca
+de serem  DINAMICO e tambem de podermos colocar qualquer tipo de dado.
+
+Linguagens C/JAVA: Arrays
+    - Possuem tamanho e tipo de dado fixo, ou seja,  nestas linguagens se voce criar um array
+    do tipo int e com tamanho 3, este array sera SEMPRE do tipo inteiro e podera ser SEMPRE no
+    maximo 5 valores.
+
+Ja em Python
+    - Dinamico nao posssui tamanho fixo: ou seja, podemos criar a lista e ir adicionando elemenmtos.
+    Nao possui tipo de dado fixo.
+As listas em Python sao rerpresentados por colchetes: []
 """
